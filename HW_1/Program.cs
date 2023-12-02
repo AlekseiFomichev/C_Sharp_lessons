991b075c39d947d9ba871714d16f7545ec6a494f
fomic <fomic@CARVERHARDWARE>
2023-12-02 11:18:48 +0300

    Добавьте файлы проекта.

---
+++ HW_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace HW_1
+{
+    //1 задание
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello World!!! ");
+            Console.ReadKey();                  
+        }
+    }
+    
+
+     // 2 задание
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.Write(" Hello ");
+    //        Console.Write(" World ");
+    //        Console.Write(" !!! ");
+
+    //        Console.ReadLine();
+
+    //    }
+    //}
+    
+}
+
